ca5719f686bb63017a9c09e9ef116b838fd12ce3
youth5427 <youth5427@jbnu.ac.kr>
2025-10-01 17:30:41 -0400

    styled Hear design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,43 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+import { FiUser, FiShoppingCart, FiSearch } from "react-icons/fi";
+import "./Header.css";
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/Menu">MENU</Link>
-        </li>
-        <li>
-          <Link to="/About">ABOUT</Link>
-        </li>
-        <li>
-          <Link to="/BookTable">BOOKTABLE</Link>
-        </li>
-      </nav>
+    <header className="header">
+      <div className="header-inner">
+        <p className="left">Icon</p>
+        <nav className="mid">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="/Menu">MENU</NavLink>
+            </li>
+            <li>
+              <NavLink to="/About">ABOUT</NavLink>
+            </li>
+            <li>
+              <NavLink to="/BookTable">BOOKTABLE</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <div className="right">
+          <button className="icon-btn">
+            <FiUser />
+          </button>
+          <button className="icon-btn">
+            <FiShoppingCart />
+          </button>
+          <button className="icon-btn">
+            <FiSearch />
+          </button>
+          <NavLink to="/" className="order">
+            Order Online
+          </NavLink>
+        </div>
+      </div>
     </header>
   );
 }
