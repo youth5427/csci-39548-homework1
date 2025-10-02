2746c96fad823eb49525670d1e19c5c139b2a75b
youth5427 <youth5427@jbnu.ac.kr>
2025-10-02 01:25:18 -0400

    add hero

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,28 @@
+import { NavLink } from "react-router-dom";
+import "./Home.css";
+
 function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-      <p>This is the Home page.</p>
+    <div className="hero">
+      <div className="hero-inner">
+        <div className="hero-intro">
+          <h1>Fast Food Restaurant</h1>
+          <p>
+            Doloremque, itaque aperiam facilis rerum, commodi, temporibus
+            sapiente ad mollitia laborum quam quisquam esse error unde. Tempora
+            ex doloremque, labore, sunt repellat dolore, iste magni quos nihil
+            ducimus libero ipsam.
+          </p>
+          <NavLink to="/" className="order">
+            Order Now
+          </NavLink>
+          <div className="dots" role="tablist" aria-label="slides">
+            <button className="dot active" aria-label="slide 1"></button>
+            <button className="dot" aria-label="slide 2"></button>
+            <button className="dot" aria-label="slide 3"></button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
