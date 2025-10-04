f8bf0be5717ac845b8c64b57ff571747fe9052ba
youth5427 <youth5427@jbnu.ac.kr>
2025-10-04 01:13:54 -0400

    fix publish bug

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,7 +3,7 @@ import "./Profile.css";
 function Header() {
   return (
     <div className="profile-section">
-      <img className="about-logo" src="/icon.png"></img>
+      <img className="about-logo" src="/icon.png" alt="Grilld_icon"></img>
       <h1>Grilld</h1>
       <p className="about-info">
         695 Park Ave, New York, NY 10065<br></br>
