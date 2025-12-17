f4c928698059c0bb38518b68b1048087c7e3f78c
youth5427 <SUHYEON.YOO33@myhunter.cuny.edu>
2025-12-17 17:08:35 -0500

    add cache header preventiion

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -12,6 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store");
+  next();
+});
+
 // connect to MongoDB
 let isConnected = false;
 async function connectDB() {
